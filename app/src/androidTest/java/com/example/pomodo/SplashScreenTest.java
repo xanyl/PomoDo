db853d958520b82c0504f02e7eb12377b58f8c90
xanyl <aniltiwari645@gmail.com>
2023-02-26 11:53:04 +0545

    Theme setting changed!

---
+++ app/src/androidTest/java/com/example/pomodo/SplashScreenTest.java
@@ -0,0 +1,23 @@
+package com.example.pomodo;
+
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class SplashScreenTest {
+
+    @Rule
+    public ActivityScenarioRule<SplashScreen> mActivityScenarioRule =
+            new ActivityScenarioRule<>(SplashScreen.class);
+
+    @Test
+    public void splashScreenTest() {
+    }
+}
